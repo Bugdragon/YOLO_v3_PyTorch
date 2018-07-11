a7f7e431c36eccabc24f0951988789566ce58a70
bugdragon <465002300@qq.com>
2018-07-11 22:25:46 +0800

    darknet

--- darknet.py
+++ darknet.py
@@ -139,8 +139,72 @@ def create_modules(blocks):
 
   return (net_info, module_list)
 
-blocks = parse_cfg("cfg/yolov3.cfg")
-print(create_modules(blocks))
+# blocks = parse_cfg("cfg/yolov3.cfg")
+# print(create_modules(blocks))
+
+class Darknet(nn.Module):
+  # 用net_info和module_list对网络进行初始化
+  def __init__(self, cfgfile):
+    super(Darknet, self).__init__()
+    self.blocks = parse_cfg(cfgfile)
+    self.net_info, self.module_list = create_modules(self.blocks)
+
+  def forward(self, x, CUDA):
+    modules = self.blocks[1:]
+    outputs = {}
+
+    write = 0
+    for i, module in enumerate(modules):
+      module_type = (module["type"])
+
+      if module_type == "convolutional" or module_type = "upsample":
+        x = self.module_list[i](x)
+
+      elif module_type == "route":
+        layers = module["layers"]
+        layers = [int(a) for a in layers]
+
+        if layers[0] > 0:
+          layers[0] = layers[0] - i
+
+        if len(layers) == 1:
+          x = outputs[i + layers[0]]
+
+        else:
+          if layers[1] > 0:
+            layers[1] = layers[1] - i
+
+          map1 = outputs[i + layers[0]]
+          map2 = outputs[i + layers[1]]
+          x = torch.cat((map1, map2), 1)
+
+      elif module_type == "shortcut":
+        from_ = int(module["from"])
+        x = outputs[i-1] + outputs[i+from_]
+
+      elif module_type == "yolo":
+        anchors = self.module_list[i][0].anchors
+        # input dimensions
+        inp_dim = int(self.net_info["height"])
+        
+        # number of classes
+        num_classes = int(module["classes"])
+
+        # transform
+        x = x.data
+        x = predict_transform(x, inp_dim, anchors, num_classes, CUDA)
+        if not write:
+          detections = x
+          write = 1
+
+        else:
+          detections = torch.cat((detections, x), 1)
+      
+      outputs[i] = x
+      
+    return detections
+
+  
 
 
 
